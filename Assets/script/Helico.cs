2d20dbd8fbe6d5ed2fd76288dccec867e1850b76
FLXGloutfou <felix.hautin@hotmail.fr>
2024-03-24 23:44:41 +0100

    helico

---
+++ Assets/script/Helico.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Helico : MonoBehaviour
+{
+    public GameObject player;
+    public Transform playerExitPoint;
+    public Transform playerSitPoint;
+    public GameObject vehicle;
+    public GameObject playerModel;
+    public GameObject pet;
+    public float vehicleMoveSpeed = 5f; 
+
+    private bool isPlayerInside = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isPlayerInside)
+                EnterVehicle();
+            else
+                ExitVehicle();
+        }
+
+        if (isPlayerInside)
+        {
+            // Contrôles du véhicule
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f).normalized;
+            vehicle.transform.position += movement * Time.deltaTime * vehicleMoveSpeed;
+        }
+    }
+
+    void EnterVehicle()
+    {
+        pet.SetActive(false);
+        playerModel.SetActive(false);
+        player.transform.position = playerSitPoint.position;
+        player.transform.parent = vehicle.transform;
+        isPlayerInside = true;
+    }
+
+    void ExitVehicle()
+    {
+        pet.SetActive(true);
+        playerModel.SetActive(true); 
+        player.transform.parent = null;
+        player.transform.position = playerExitPoint.position;
+        isPlayerInside = false;
+    }
+}
