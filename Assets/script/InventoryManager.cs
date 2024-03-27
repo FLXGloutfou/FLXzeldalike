0401c8c1b43b2823ed56eb78223a0861c266477b
Félix <felix.hautin@hotmail.fr>
2024-03-27 16:23:20 +0100

    début inventaire et première item

---
+++ Assets/script/InventoryManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("inventory") && menuActivated)
+        {
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else if (Input.GetButtonDown("inventory") && !menuActivated)
+        {
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
+
+
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
+    {
+        Debug.Log("itemName =" + itemName + "quantity =" + quantity + "itemSprite = " + itemSprite);
+    }
+}
---
+++ Assets/script/Item.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+
+    [SerializeField]
+    private int quantity;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    private InventoryManager inventoryManager;
+    void Start()
+    {
+        inventoryManager = GameObject.Find("inventory").GetComponent<InventoryManager>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            inventoryManager.AddItem(itemName, quantity, sprite);
+            Destroy(gameObject);
+        }
+
+    }
+
+}
+
+
